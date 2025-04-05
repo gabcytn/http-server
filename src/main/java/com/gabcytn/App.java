c3f78c6b1d60d4afdca402766f8daa5bc203a6e2
gabcytn <cayetanogabriel03@gmail.com>
2025-04-05 22:50:43 +0800

    feat: read & log http request and send today's date as http response

--- src/main/java/com/gabcytn/App.java
+++ src/main/java/com/gabcytn/App.java
@@ -1,13 +1,40 @@
 package com.gabcytn;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+public class App
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException
     {
-        System.out.println( "Hello World!" );
+        ServerSocket serverSocket = new ServerSocket(8080);
+        System.out.println("Server is listening at port 8080...");
+        while (true)
+        {
+            try (Socket socket = serverSocket.accept())
+            {
+                BufferedReader bufferedReader =
+                        new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                String line = bufferedReader.readLine();
+                while (!line.isEmpty())
+                {
+                    System.out.println(line);
+                    line = bufferedReader.readLine();
+                }
+                Date date = new Date();
+                String response = "HTTP/1.1 200 OK\r\n\r\n" + date;
+                socket.getOutputStream()
+                        .write(response.getBytes(StandardCharsets.UTF_8));
+            }
+            catch (IOException e)
+            {
+                System.err.println("IOException: " + e.getMessage());
+            }
+        }
     }
 }
