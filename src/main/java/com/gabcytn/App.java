a27c1dc239b6280fa402a71276eae65b8c11bfe2
gabcytn <cayetanogabriel03@gmail.com>
2025-04-07 01:03:32 +0800

    refactor: store request headers in a map

--- src/main/java/com/gabcytn/App.java
+++ src/main/java/com/gabcytn/App.java
@@ -7,9 +7,16 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class App
 {
+    final static String HTTP_VERSION = "HTTP/1.1";
+    final static String CRLF = "\r\n\r\n";
+
+    final static Map<String, String> requestHeaders = new HashMap<>();
+
     public static void main( String[] args ) throws IOException
     {
         ServerSocket serverSocket = new ServerSocket(8080);
@@ -20,14 +27,22 @@ public class App
             {
                 BufferedReader bufferedReader =
                         new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                String line = bufferedReader.readLine();
-                while (!line.isEmpty())
-                {
-                    System.out.println(line);
-                    line = bufferedReader.readLine();
-                }
-                Date date = new Date();
-                String response = "HTTP/1.1 200 OK\r\n\r\n" + date;
+
+                String[] statusLine = bufferedReader.readLine().split(" ");
+                String httpVerb = statusLine[0];
+                String requestPath = statusLine[1];
+
+                System.out.println("HTTP Verb: " + httpVerb);
+                System.out.println("Request path: " + requestPath);
+
+                readRequestHeaders(bufferedReader);
+
+                System.out.println("---------HEADERS---------");
+                for (Map.Entry<String, String> headers : requestHeaders.entrySet())
+                    System.out.println(headers.getKey() + ": " + headers.getValue());
+                System.out.println("---------HEADERS---------");
+
+                String response = HTTP_VERSION + " 200 OK" + CRLF + CRLF;
                 socket.getOutputStream()
                         .write(response.getBytes(StandardCharsets.UTF_8));
             }
@@ -35,6 +50,24 @@ public class App
             {
                 System.err.println("IOException: " + e.getMessage());
             }
+        }
+    }
+
+    private static void readRequestHeaders (BufferedReader reader)
+    {
+        String line;
+        try
+        {
+            while (!(line = reader.readLine()).isEmpty())
+            {
+                String[] header = line.split(":");
+                requestHeaders.put(header[0].trim().toLowerCase(), header[1].trim().toLowerCase());
+            }
+        }
+        catch (IOException e)
+        {
+            System.err.println("Error while reading request headers");
+            System.err.println("Message: " + e.getMessage());
         }
     }
 }
