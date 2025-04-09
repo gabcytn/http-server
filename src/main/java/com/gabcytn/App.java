7f315f3baea3c5872adcb100347c89ec29043c3e
gabcytn <cayetanogabriel03@gmail.com>
2025-04-09 11:27:03 +0800

    refactor: use ExecutorService class to handle concurrency

--- src/main/java/com/gabcytn/App.java
+++ src/main/java/com/gabcytn/App.java
@@ -5,12 +5,15 @@ import com.gabcytn.server.RequestHandler;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class App
 {
     public static void main( String[] args ) throws IOException
     {
         ServerSocket serverSocket = new ServerSocket(8080);
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
         System.out.println("Server is listening at port 8080...");
         while (true)
         {
@@ -18,8 +21,7 @@ public class App
             {
                 Socket socket = serverSocket.accept();
                 Runnable requestHandler = new RequestHandler(socket);
-                Thread thread = new Thread(requestHandler);
-                thread.start();
+                executorService.submit(requestHandler);
             }
             catch (IOException e)
             {
