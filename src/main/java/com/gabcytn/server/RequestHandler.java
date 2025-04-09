e7f3809bc0384d98ed93a0790bef677bc60ede26
gabcytn <cayetanogabriel03@gmail.com>
2025-04-09 11:16:42 +0800

    refactor: create ResponseHandler class to handle creation of http response

--- src/main/java/com/gabcytn/server/RequestHandler.java
+++ src/main/java/com/gabcytn/server/RequestHandler.java
@@ -11,7 +11,7 @@ public class RequestHandler implements  Runnable
 {
     private final Socket socket;
     private final RequestReader requestReader;
-    private static final String FILES_DIR = "files/";
+    private final ResponseHandler responseHandler;
 
     public RequestHandler (Socket socket) throws IOException
     {
@@ -19,6 +19,7 @@ public class RequestHandler implements  Runnable
         this.socket.setSoTimeout(5000);
         this.socket.setReuseAddress(true);
         this.requestReader = new RequestReader(socket.getInputStream());
+        this.responseHandler = new ResponseHandler(requestReader);
     }
 
     @Override
@@ -29,21 +30,13 @@ public class RequestHandler implements  Runnable
             requestReader.read();
             String response;
             if (requestReader.getRequestPath().startsWith("/echo/"))
-                response = handleEcho();
+                response = responseHandler.handleEcho();
             else if (requestReader.getRequestPath().startsWith("/file/"))
-                response = readFile();
-            else if ("/".equals(requestReader.getRequestPath())) {
-                response = new ResponseBuilder()
-                        .setHttpVersion(requestReader.getHttpVersion())
-                        .setStatusCode(200)
-                        .setStatus("OK")
-                        .setHeader("Content-Length", "0")
-                        .setHeader("Connection", "close")
-                        .build()
-                            .toString();
-            }
+                response = responseHandler.readFile();
+            else if ("/".equals(requestReader.getRequestPath()))
+                response = responseHandler.generate200WithoutBody();
             else
-                response = generate404();
+                response = responseHandler.generate404();
             socket.getOutputStream().write(response.getBytes(StandardCharsets.UTF_8));
             socket.close();
         }
@@ -51,82 +44,5 @@ public class RequestHandler implements  Runnable
         {
             System.err.println("IOException in run() method: " + e.getMessage());
         }
-    }
-
-    private String handleEcho ()
-    {
-        String[] paths = requestReader.getRequestPath().split("/");
-        if (paths.length != 3)
-            return generate404();
-
-        String path = paths[2];
-        return new ResponseBuilder()
-                .setHttpVersion(requestReader.getHttpVersion())
-                .setStatusCode(200)
-                .setStatus("OK")
-                .setHeader("Content-Type", "text/plain")
-                .setHeader("Content-Length", Integer.toString(path.length()))
-                .setHeader("Connection", "close")
-                .setBody(path)
-                .build()
-                    .toString();
-    }
-
-    private String readFile()
-    {
-        String[] paths = requestReader.getRequestPath().split("/");
-        if (paths.length != 3)
-            return generate404();
-
-        String fileName = paths[2];
-        String fileContent = readFile(fileName);
-        if (fileContent == null)
-            return generate404();
-        return new ResponseBuilder()
-                .setHttpVersion(requestReader.getHttpVersion())
-                .setStatusCode(200)
-                .setStatus("OK")
-                .setHeader("Content-Type", "text/plain")
-                .setHeader("Content-Length", Integer.toString(fileContent.length()))
-                .setHeader("Connection", "close")
-                .setBody(fileContent)
-                .build()
-                    .toString();
-    }
-
-    private String readFile (String fileName)
-    {
-        try
-        {
-            File file = new File(FILES_DIR + fileName);
-            if (!file.exists())
-                throw new FileNotFoundException(fileName + " not found");
-            BufferedReader reader = new BufferedReader(new FileReader(file));
-            StringBuilder stringBuilder = new StringBuilder();
-            String line = reader.readLine();
-            stringBuilder.append(line);
-            while ((line = reader.readLine()) != null)
-                stringBuilder.append("\n").append(line);
-            return stringBuilder.toString();
-        }
-        catch (IOException e)
-        {
-            System.err.println("File not found: " + e.getMessage());
-            e.printStackTrace();
-            return null;
-        }
-
-    }
-
-    private String generate404 ()
-    {
-        return new ResponseBuilder()
-                .setHttpVersion(requestReader.getHttpVersion())
-                .setStatusCode(404)
-                .setStatus("Not Found")
-                .setHeader("Content-Length", "0")
-                .setHeader("Connection", "close")
-                .build()
-                    .toString();
     }
 }
---
+++ src/main/java/com/gabcytn/server/ResponseHandler.java
@@ -0,0 +1,105 @@
+package com.gabcytn.server;
+
+import com.gabcytn.http.RequestReader;
+import com.gabcytn.http.ResponseBuilder;
+
+import java.io.*;
+
+
+public class ResponseHandler {
+    private final RequestReader requestReader;
+    private static final String FILES_DIR = "files/";
+
+    public ResponseHandler (RequestReader requestReader) {
+        this.requestReader = requestReader;
+    }
+
+    public String handleEcho ()
+    {
+        String[] paths = requestReader.getRequestPath().split("/");
+        if (paths.length != 3)
+            return generate404();
+
+        String path = paths[2];
+        return new ResponseBuilder()
+                .setHttpVersion(requestReader.getHttpVersion())
+                .setStatusCode(200)
+                .setStatus("OK")
+                .setHeader("Content-Type", "text/plain")
+                .setHeader("Content-Length", Integer.toString(path.length()))
+                .setHeader("Connection", "close")
+                .setBody(path)
+                .build()
+                .toString();
+    }
+
+    public String readFile()
+    {
+        String[] paths = requestReader.getRequestPath().split("/");
+        if (paths.length != 3)
+            return generate404();
+
+        String fileName = paths[2];
+        String fileContent = readFile(fileName);
+        if (fileContent == null)
+            return generate404();
+        return new ResponseBuilder()
+                .setHttpVersion(requestReader.getHttpVersion())
+                .setStatusCode(200)
+                .setStatus("OK")
+                .setHeader("Content-Type", "text/plain")
+                .setHeader("Content-Length", Integer.toString(fileContent.length()))
+                .setHeader("Connection", "close")
+                .setBody(fileContent)
+                .build()
+                .toString();
+    }
+
+    private String readFile (String fileName)
+    {
+        try
+        {
+            File file = new File(FILES_DIR + fileName);
+            if (!file.exists())
+                throw new FileNotFoundException(fileName + " not found");
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            StringBuilder stringBuilder = new StringBuilder();
+            String line = reader.readLine();
+            stringBuilder.append(line);
+            while ((line = reader.readLine()) != null)
+                stringBuilder.append("\n").append(line);
+            return stringBuilder.toString();
+        }
+        catch (IOException e)
+        {
+            System.err.println("File not found: " + e.getMessage());
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public String generate404 ()
+    {
+        return new ResponseBuilder()
+                .setHttpVersion(requestReader.getHttpVersion())
+                .setStatusCode(404)
+                .setStatus("Not Found")
+                .setHeader("Content-Length", "0")
+                .setHeader("Connection", "close")
+                .build()
+                .toString();
+    }
+
+    public String generate200WithoutBody ()
+    {
+
+        return new ResponseBuilder()
+                .setHttpVersion(requestReader.getHttpVersion())
+                .setStatusCode(200)
+                .setStatus("OK")
+                .setHeader("Content-Length", "0")
+                .setHeader("Connection", "close")
+                .build()
+                    .toString();
+    }
+}
