33972b53485f1835383ae7edc941c5abbb0fc46c
gabcytn <cayetanogabriel03@gmail.com>
2025-04-13 21:59:05 +0800

    test: root endpoint of server

--- src/test/java/com/gabcytn/AppTest.java
+++ src/test/java/com/gabcytn/AppTest.java
@@ -4,6 +4,11 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
+import java.io.*;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Unit test for simple App.
  */
@@ -29,10 +34,40 @@ public class AppTest
     }
 
     /**
-     * Rigourous Test :-)
+     * Rigorous Test :-)
      */
-    public void testApp()
+    public void testRootEndpoint()
     {
-        assertTrue( true );
+        try (Socket socket = new Socket("localhost", 8080))
+        {
+            OutputStream outputStream = socket.getOutputStream();
+            PrintWriter writer = new PrintWriter(outputStream, true);
+            writer.println("GET / HTTP/1.1\r\n\r\n");
+
+            BufferedReader reader =
+                    new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            // Read status line
+            String response = reader.readLine();
+            assertEquals("HTTP/1.1 200 OK", response);
+
+            // Read headers
+            Map<String, String> headers = new HashMap<>();
+            String line;
+            while (!(line = reader.readLine()).isEmpty()) {
+                String[] header = line.split(":", 2);
+                headers.put(header[0].trim().toLowerCase(), header[1].trim().toLowerCase());
+            }
+
+            assertNotNull(headers.get("connection"));
+            assertNotNull(headers.get("content-length"));
+
+            assertEquals("keep-alive", headers.get("connection"));
+            assertEquals("0", headers.get("content-length"));
+        }
+        catch (IOException e)
+        {
+            System.err.println("Error opening socket, " + e.getMessage());
+        }
     }
 }
